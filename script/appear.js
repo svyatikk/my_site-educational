bc9f3ad546d80d23ff7bc47b2c2901b77cdaccaa
svyatikk <svyatoslavgolovin12@gmail.com>
2024-07-28 15:10:11 +0300

    Add files via upload

---
+++ script/appear.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let block = document.querySelector(".appear");
+    let block2 = document.querySelector(".reveal-x");
+    block.classList.add("active");
+    block2.classList.add("active");
+};
--- script/script.js
+++ script/script.js
@@ -1,53 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var burgerIcon = document.getElementById("burger-icon");
-    var sideMenu = document.getElementById("side-menu");
-    var overlay = document.getElementById("overlay");
+let menuBurger = document.querySelector('.menu-burger');
+let headerBurger = document.querySelector('.header-burger');
 
-    function toggleMenu() {
-        sideMenu.classList.toggle("active");
-        overlay.classList.toggle("active");
-    }
-
-    burgerIcon.addEventListener("click", toggleMenu);
-    overlay.addEventListener("click", toggleMenu);
+menuBurger.addEventListener('click', () => {
+    headerBurger.classList.toggle('menu-open');
 });
 
-function slider() {
-
-}
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "flex";
-    document.getElementById("count").innerText = slideIndex;
-}
-
-function toggleBlockVisibility() {
-
-    document.getElementById('showPhoneNumberForm').addEventListener('click', function () {
-        document.getElementById('phoneNumberForm').style.display = 'block';
+let navLinks = document.querySelectorAll('.nav-item-burger a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        headerBurger.classList.remove('menu-open');
     });
-
-    document.getElementById('hidePhoneNumberForm').addEventListener('click', function () {
-        document.getElementById('phoneNumberForm').style.display = 'none';
-    });
-
-}
-toggleBlockVisibility();
+});
